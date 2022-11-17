8e9ccf4b59cd2cf55467f51c250238caee679ede
Gabriel-Robles-Cueva <113194750+Gabriel-Robles-Cueva@users.noreply.github.com>
2022-11-17 19:48:04 +0100

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -30,9 +30,9 @@ session_start();
 		for ($i=0; $i < count($_SESSION["cartasMesa"]); $i++) { 
 			sort($_SESSION['cartasMesa']);
 			echo "<div class='cartaMesa'>	
-			<input type='radio' name='tablero' id='1' class='left' value='left'>";
+			<input type='radio' name='tablero' id='$i' class='left' value='left'>";
 			print_r($_SESSION["cartasMesa"][$i]);
-			echo "<input type='radio' name='tablero' id='2' class='right' value='right'>
+			echo "<input type='radio' name='tablero' id='$i' class='right' value='right'>
 			</div>";
 		}
 		?>
@@ -40,8 +40,7 @@ session_start();
 	</div>
 
 	<?php  
-		function colocar()
-		{
+		if (isset($_POST['tablero'])) {
 			
 		}
 	?>
