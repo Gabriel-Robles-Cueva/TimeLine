0e158c8e3db0591ebe3f66969304dfa23eb723ed
Daniel Medrano <56469494+KaxelMed@users.noreply.github.com>
2022-11-17 19:47:00 +0100

    l

--- inicio.php
+++ inicio.php
@@ -5,12 +5,16 @@
 	$_SESSION["cartasMesa"]= array();
 	$_SESSION["cartasMano"]= array();
 	shuffle($_SESSION["baraja"]);
+    $_SESSION["cartasMesa"][]= array_shift($_SESSION["baraja"]);
+	for($i=0; $i<3; $i++){
+		$_SESSION["cartasMano"][] = array_shift($_SESSION["baraja"]);
+	}
 ?>
 <html>
     <body>
         <form action="timeline.php">
         <h1 align = center> Bienvendio</h1>
-        <submit value="Empecemos"/>
+        <input type=submit value="Empecemos"/>
         </form>
     </body>
 </html>
