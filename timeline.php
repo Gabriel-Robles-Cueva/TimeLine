d9e96da67e38bba520ab9d047d7bdcb1a9025ace
Gabriel-Robles-Cueva <113194750+Gabriel-Robles-Cueva@users.noreply.github.com>
2022-11-21 00:02:08 +0100

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -1,8 +1,5 @@
 <?php 
 session_start();
-
-	
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,50 +15,47 @@ session_start();
 		ANTIGUO
 	</div>
 	<form action="timeline.php" method="post">
-	<div class="cartasMesa">
-		<?php
-		for ($i=0; $i < count($_SESSION["cartasMesa"]); $i++) { 
-			sort($_SESSION['cartasMesa']);
-			echo "<div class='cartaMesa'>	
-			<input type='radio' name='tablero' id='$i' class='left' value='left'>";
-			print_r($_SESSION["cartasMesa"][$i]);
-			echo "<input type='radio' name='tablero' id='$i' class='right' value='right'>
-			</div>";
-		}
-		?>
-		
-	</div>
-
-	<?php  
-		function colocar()
-		{
-			if (isset($_POST['tablero']) and isset($_POST['cartaMano'])) {
-				echo "<h1>AAAAAAAAAAAA</h1>";
+		<div class="cartasMesa">
+			<?php
+			for ($i=0; $i < count($_SESSION["cartasMesa"]); $i++) { 
+				sort($_SESSION['cartasMesa']);
+				echo "<div class='cartaMesa'>";
+				?>	
+				<input type='radio' name='tablero' id='tablero' class='left' value='left'>
+				<?php 
+				print_r($_SESSION["cartasMesa"][$i]);
+				?>
+				<input type='radio' name='tablero' id='tablero' class='right' value='right'>
+				</div>
+			<?php
 			}
+			?>			
+		</div>
+		<?php
+			$valorTablero= $_SESSION['tablero'];
+		?>
+
+
+		<div class="nuevo">
+				NUEVO
+		</div>
+		<div class="misCartas">
+		<?php
+		
+		for ($i=0; $i < count($_SESSION['cartasMano']); $i++) {
+			$robo = $_SESSION['cartasMano'][$i];
+			echo '<div class="cartaMano" onclick="">';
+			echo "<input name='cartaMano' type='radio' name='test' value='$robo'>";
+			print_r($_SESSION['cartasMano'][$i]);
+			echo'</div>';
 		}
-	?>
+		/* do{
+		}while(count($_SESSION["baraja"])!=0);  */
+		?>
 
+			<button onclick="colocar()">COLOCAR</button>
 
-	<div class="nuevo">
-			NUEVO
-	</div>
-	<div class="misCartas">
-	<?php
-	
-	for ($i=0; $i < count($_SESSION['cartasMano']); $i++) {
-		$robo = $_SESSION['cartasMano'][$i];
-		echo '<div class="cartaMano" onclick="">';
-		echo "<input name='cartaMano' type='radio' name='test' value='$robo'>";
-		print_r($_SESSION['cartasMano'][$i]);
-		echo'</div>';
-	}
-	/* do{
-	}while(count($_SESSION["baraja"])!=0);  */
-	?>
-
-		<button onclick="colocar()">COLOCAR</button>
-
-	</div>
-</form>
+		</div>
+	</form>
 </body>
 </html>
